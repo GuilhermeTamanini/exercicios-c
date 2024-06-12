e43e9b99d06f1497db2fec55b65fa4d567f138b4
guilherme <guilherme.gui.tama@gmail.com>
2024-06-12 20:05:38 -0300

    3 sendo feita

--- lista05/lista05.c
+++ lista05/lista05.c
@@ -10,7 +10,7 @@ int main() {
     scanf("%i", &num);
 
     printf("Numero par: 0\nNumero impar: 1\nNumero: %i", isOdd(num));
-    
+
     return 0;
 }
 
@@ -41,7 +41,7 @@ int main() {
     return 0;
 }
 
-/* 3. Fazer um programa que possibilite várias opções de cálculos a partir de um 
+/* 3. Fazer um programa que possibilite várias opções de cálculos a partir de um
  menu. O programa chamará a função correspondente a cada cálculo.
  [ a ] S = 1/1 + 3/2 + 5/3+........+ 99/50
  [ b ] S = 1/1 - 2/2 + 3/3 -..........- 10/10
@@ -51,10 +51,10 @@ int main() {
  Obs.: Nas opções [c] e [d] fazer os cálculos para os 20 primeiros termos.*/
 
 int main() {
-    char option;
+    char option, v = 1;
 
     do{
-        printf("[ a ] S = 1/1 + 3/2 + 5/3+........+ 99/5\n");
+        printf("\n[ a ] S = 1/1 + 3/2 + 5/3+........+ 99/50\n");
         printf("[ b ] S = 1/1 - 2/2 + 3/3 -..........- 10/10\n");
         printf("[ c ] S = 1000/1 - 997/2 + 994/3........\n");
         printf("[ d ] S = 480/10 - 475/11 + 470/12 - .......\n");
@@ -62,9 +62,26 @@ int main() {
 
         printf("Digite uma opcao: ");
         scanf("%c", &option);
-        if(option > 'a' && option < 'f') {
-            
+        fflush(stdin);
+        if(option >= 'a' && option <= 'f') {
+            switch (option){
+                case 'a':
+                    for(int i=1; i<=50; i++) {
+                        printf("%i/%i", v, i);
+                        v += 2;
+                        if(i != 50) printf(" + ");
+                    }
+                    break;
+                case 'b':
+                    for(int i=1; i<=10; i++) {
+                        printf("%i/%i", i, i);
+                        if(i != 10) printf(" - ");
+                    }
+                    break;
+                default:
+                    printf("Opcao invalida");
+                    break;
+                }
         }
     } while(option != 'f');
-    
 }
